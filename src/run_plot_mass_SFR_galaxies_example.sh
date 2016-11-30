d2c18dccd580922c18c82743b0f65d60d79ed76c
jacopo-chevallard <jacopo.chevallard@gmail.com>
2016-11-30 11:34:06 +0100

    Example bash script to run the 'plot_mass_SFR_galaxies.py' script on multiple inputs

---
+++ src/run_plot_mass_SFR_galaxies_example.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/WG3_z_lt_4_mock_test_5_new
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/UVUDF/Emma_IDs/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_new --suffix _min_z_1.0
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/UVUDF/Emma_IDs/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian --suffix _min_z_1.0
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/UVUDF/Emma_IDs/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian --suffix _min_z_1.0_min_M_4.0e6
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/XDF/B_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_corrected_z_form  --suffix _min_z_2.0
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/XDF/V_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_corrected_z_form  --suffix _min_z_3.0
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/XDF/I_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_corrected_z_form  --suffix _min_z_4.0
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/XDF/Z_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_corrected_z_form  --suffix _min_z_5.0
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/XDF/Y_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_corrected_z_form  --suffix _min_z_5.0
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/XDF/B_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_corrected_z_form  --suffix _min_z_2.0_min_M_1.0e7
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/XDF/V_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_corrected_z_form  --suffix _min_z_3.0_min_M_1.5e7
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/XDF/I_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_corrected_z_form  --suffix _min_z_4.0_min_M_2.5e7
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/XDF/Z_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_corrected_z_form  --suffix _min_z_5.0_min_M_2.5e7
+
+#python plot_mass_SFR_galaxies.py -r /Users/jchevall/JWST/Simulations/XDF/Y_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_corrected_z_form  --suffix _min_z_5.0_min_M_2.5e7
