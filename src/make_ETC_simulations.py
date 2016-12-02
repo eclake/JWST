66f1e1d6b599008a27f6a5911253d0a266c71757
beagle-machine <jacopo.chevallard@esa.int>
2016-12-02 16:38:38 +0100

    Added multi-processing capabilities

--- src/make_ETC_simulations.py
+++ src/make_ETC_simulations.py
@@ -16,6 +16,12 @@ import matplotlib.pyplot as plt
 import argparse
 import re
 
+import sys
+sys.path.append(os.path.join(os.environ['PYP_BEAGLE'], "PyP-BEAGLE"))
+import beagle_multiprocess
+
+from pathos.multiprocessing import ProcessingPool 
+
 c_light = 2.99792e+18 # Ang/s
 show_plot = False
 
@@ -24,6 +30,9 @@ jwstpytools = os.environ['JWSTPYTOOLS']
 jwstpytools_procedure = os.path.join(jwstpytools, "sensitivity/p_spectrumMOS1x3.py")
 jwstpytools_data = os.path.join(jwstpytools, "data")
 pce = "PCE-NIRS30-IFU30-FPA106"
+
+ETC_output_dir = ""
+ETC_input_dir = ""
 
 # How many exposures?
 #nbexps = ("108", "36", "36", "36")
@@ -112,6 +121,78 @@ def write_ETC_input_file(wl, flux, redshift, file_name):
     # Write the FITS file containing the input file for Pierre's routines
     ETC_spectrum.m_writeToSimpleFITS(file_name)
 
+
+def make_ETC_simulations_single(ETC_simulation_prefix, 
+        wl, sed, redshift,
+        recompute, 
+        FWAs, GWAs, nbexps):
+
+    # SED (units are those putput from Beagle, i.e. erg s^-1 cm^-2 A^-1)
+    # Redshfit of the i-th object (i.e., i-th row in the input FITS catalogue)
+
+    # Name of the FITS file containing the input SED for the ETC simulator
+    ETC_input_file = os.path.join(ETC_input_dir, ETC_simulation_prefix + '_input_for_ETC.fits')
+
+    # By default you always recompute the input file for the ETC, but in
+    # some occasions ypu may just want to create the input file for some
+    # missing objects 
+    if not os.path.isfile(ETC_input_file) or recompute:
+
+        # Function that creates the FITS file that will later be used as
+        # input for the ETC simulator. Note that this function simply
+        # convert the flux to observed frame, and from F_lambda into F_nu
+        # (in Jansky)
+        write_ETC_input_file(wl, sed, redshift, ETC_input_file)
+
+    # Cycle across each combination of filter, grating, and number of exposures
+    for FWA, GWA, nbexp in zip(args.FWAs, args.GWAs, args.nbexps):
+
+        # Name of the file created by the ETC simulator (need the name to
+        # check if the file already exists or not)
+        ETC_output_file = ETC_simulation_prefix + "_snr_PS_" + FWA + "_" + GWA + ".fits"
+        ETC_output_file = os.path.join(ETC_output_dir, ETC_output_file)
+        if not os.path.isfile(ETC_output_file) or recompute:
+
+            # Run the actual scripts that compute the ETC-like simulated NIRSpec observation
+            compute_ETC_simulation(ETC_input_file, FWA, GWA, nbexp, ETC_output_dir, ETC_simulation_prefix)
+
+            # The SED output from the ETC simulator is in units of Jansky
+            # (F_nu), while Beagle works in F_lambda. We therefore add two
+            # columns to the ETC output containing a Beagle-friendly
+            # format.
+
+            # Open the file containing the ETC-like simulation
+            hduETC = fits.open(ETC_output_file)
+
+            # Get existing columns
+            existing_cols = hduETC[1].columns
+
+            # Add new columns
+            new_col = list()
+
+            # Add "FLUX_FLAMBDA" column, expressing the flux in F_lambda, erg s^-1 cm^-2 A^-1
+            # The units of the NRSPEC column are Jy
+            flux = hduETC[1].data['NRSPEC'] * 1.E-23 * c_light / (hduETC[1].data['WAVELENGTH']*1.E+10)**2
+            new_col.append(fits.Column(name='FLUX_FLAMBDA', array=flux, format='E', unit='erg s^-1 cm^-2 A^-1'))
+
+            # Add "NOISE_FLAMBDA" column, expressing the flux in F_lambda, erg s^-1 cm^-2 A^-1
+            # The units of the NOISE column are Jy
+            noise = hduETC[1].data['NOISE'] * 1.E-23 * c_light / (hduETC[1].data['WAVELENGTH']*1.E+10)**2
+            new_col.append(fits.Column(name='NOISE_FLAMBDA', array=noise, format='E', unit='erg s^-1 cm^-2 A^-1'))
+
+            new_col_defs = fits.ColDefs(new_col)
+
+            hduETC[1] = fits.BinTableHDU.from_columns(existing_cols+new_col_defs)
+
+            # Add redshift keyword
+            hduETC[1].header['redshift'] = float(redshift)
+
+            # Overwrite the FITS file
+            hduETC.writeto(ETC_output_file, clobber=True)
+
+            hduETC.close()
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -166,6 +247,15 @@ if __name__ == '__main__':
         dest="nbexps", 
         nargs='+',
         default=("108",)
+    )
+
+    parser.add_argument(
+        '-nproc',
+        help="Number of processors to use",
+        action="store", 
+        type=int, 
+        dest="nproc",
+        default=-1
     )
 
     parser.add_argument(
@@ -257,91 +347,71 @@ if __name__ == '__main__':
     # Get the redshifts of the different SEDs
     redshifts = hdulist['galaxy properties'].data['redshift']
 
+    # Get the SEDs
+    SEDs_ = list()
+    for i in range(hdulist['full sed'].data[:,:].shape[0]):
+        SEDs_.append(hdulist['full sed'].data[i,:])
+
+    hdulist.close()
+
     # By default you create simulated NIRSpec observations for all the objects
     # in the catalogue, but the user can choose to just run on the first N
     # objects (mainly for testing purposes!)
     n_objects = len(redshifts)
-    rows = range(n_objects)    
+    rows = np.array(range(n_objects), dtype=int)
+
+    # If requested, shuffle the roder of the rows
     if args.shuffle:
         random.shuffle(rows)
 
+    # The user can choose to compute the simulated spectra only for a subset of
+    # the possible input SEDs
     if args.N:
         rows = rows[:args.N]
 
-    # Cycle across all objects in the input FITS catalogue
+    # Re-ordert the SEDs_ list to follow the suffled order of the rows
+    SEDs = list()
     for row in rows:
+        SEDs.append(SEDs_[row])
 
-        # Redshfit of the i-th object (i.e., i-th row in the input FITS catalogue)
-        redshift = redshifts[row]
+    # Re-ordert the redshifts array to follow the suffled order of the rows
+    redshifts = redshifts[rows]
 
-        # Name of the FITS file containing the input SED for the ETC simulator
-        ETC_input_file = os.path.join(ETC_input_dir, str(row+1) + suffix + '_input_for_ETC.fits')
+    # Create a list containing the prefix used for the input and output file
+    # for the ETC simulator
+    ETC_simulation_prefixes = list()
+    for row in rows:
+        ETC_simulation_prefixes.append(str(row+1) + suffix)
 
-        # By default you always recompute the input file for the ETC, but in
-        # some occasions ypu may just want to create the input file for some
-        # missing objects 
-        if not os.path.isfile(ETC_input_file) or recompute:
+    # If the user does not specify the number of processors to be used, assume that it is a serial job
+    if args.nproc <= 0:
 
-            # SED (units are those putput from Beagle, i.e. erg s^-1 cm^-2 A^-1)
-            sed = hdulist['full sed'].data[row,:]
+        for i in range(len(rows)):
+             make_ETC_simulations_single(
+                ETC_simulation_prefix=ETC_simulation_prefixes[i],
+                wl=wl,
+                sed=SEDs[i],
+                redshift=redshifts[i],
+                recompute=recompute,
+                FWAs=args.FWAs,
+                GWAs=args.GWAs,
+                nbexps=args.nbexps
+                )
+    
+    # Otherwise you use pathos to run in parallel on multiple CPUs
+    else:
 
-            # Function that creates the FITS file that will later be used as
-            # input for the ETC simulator. Note that this function simply
-            # convert the flux to observed frame, and from F_lambda into F_nu
-            # (in Jansky)
-            write_ETC_input_file(wl, sed, redshift, ETC_input_file)
+        # Set number of parellel processes to use
+        pool = ProcessingPool(nodes=args.nproc)
 
-
-        # Prefix for the output file produced by the ETC simulator
-        ETC_output_prefix = str(row+1) + suffix
-
-        # Cycle across each combination of filter, grating, and number of exposures
-        for FWA, GWA, nbexp in zip(args.FWAs, args.GWAs, args.nbexps):
-
-            # Name of the file created by the ETC simulator (need the name to
-            # check if the file already exists or not)
-            ETC_output_file = ETC_output_prefix + "_snr_PS_" + FWA + "_" + GWA + ".fits"
-            ETC_output_file = os.path.join(ETC_output_dir, ETC_output_file)
-            if not os.path.isfile(ETC_output_file) or recompute:
-
-                # Run the actual scripts that compute the ETC-like simulated NIRSpec observation
-                compute_ETC_simulation(ETC_input_file, FWA, GWA, nbexp, ETC_output_dir, ETC_output_prefix)
-
-                # The SED output from the ETC simulator is in units of Jansky
-                # (F_nu), while Beagle works in F_lambda. We therefore add two
-                # columns to the ETC output containing a Beagle-friendly
-                # format.
-
-                # Open the file containing the ETC-like simulation
-                hduETC = fits.open(ETC_output_file)
-
-                # Get existing columns
-                existing_cols = hduETC[1].columns
-
-                # Add new columns
-                new_col = list()
-
-                # Add "FLUX_FLAMBDA" column, expressing the flux in F_lambda, erg s^-1 cm^-2 A^-1
-                # The units of the NRSPEC column are Jy
-                flux = hduETC[1].data['NRSPEC'] * 1.E-23 * c_light / (hduETC[1].data['WAVELENGTH']*1.E+10)**2
-                new_col.append(fits.Column(name='FLUX_FLAMBDA', array=flux, format='E', unit='erg s^-1 cm^-2 A^-1'))
-
-                # Add "NOISE_FLAMBDA" column, expressing the flux in F_lambda, erg s^-1 cm^-2 A^-1
-                # The units of the NOISE column are Jy
-                noise = hduETC[1].data['NOISE'] * 1.E-23 * c_light / (hduETC[1].data['WAVELENGTH']*1.E+10)**2
-                new_col.append(fits.Column(name='NOISE_FLAMBDA', array=noise, format='E', unit='erg s^-1 cm^-2 A^-1'))
-
-                new_col_defs = fits.ColDefs(new_col)
-
-                hduETC[1] = fits.BinTableHDU.from_columns(existing_cols+new_col_defs)
-
-                # Add redshift keyword
-                hduETC[1].header['redshift'] = float(redshift)
-
-                # Overwrite the FITS file
-                hduETC.writeto(ETC_output_file, clobber=True)
-
-                hduETC.close()
-
-
-    hdulist.close()
+        # Launch the actual calculation on multiple processesors
+        pool.map(make_ETC_simulations_single, 
+            ETC_simulation_prefixes,
+            (wl,)*len(rows),
+            SEDs,
+            redshifts,
+            (recompute,)*len(rows),
+            (args.FWAs,)*len(rows),
+            (args.GWAs,)*len(rows),
+            (args.nbexps,)*len(rows)
+            )
