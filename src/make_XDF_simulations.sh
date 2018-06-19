6387863f8e4aa3583e48f775bc1beae7402732ef
jacopo-chevallard <jacopo.chevallard@esa.int>
2018-06-19 16:23:03 +0200

    Updated bash script to create NIRSpec simulations

--- src/make_XDF_simulations.sh
+++ src/make_XDF_simulations.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-declare -a dropouts=("B" "V" "I" "Z" "Y")
+dropouts=("B" "V" "I" "Z" "Y")
+
+MC_runs=(1 2 3 4 5 6 7 8 9)
+MC_runs=(0)
 
 # Effective radii computed by considering (in redshift bins of delta z=1) the
 # mean of the log-normal distribution of FWHM in the UDF master catalogue
 # (column UVUDF_FWHM_775_noPsf)
+# you can use the command line option
+# --effective-radius ${effective_radii[$i]}
+# to use the values below
 effective_radii=(0.10 0.11 0.12 0.13 0.145)
 
-MC_run="MC_0"
 nbexp=108
 filters="CLEAR"
 gratings="PRISM"
-sersic=1
-nproc=4
+sersic="1.5" # from Shibuya+2015
+nproc=22
 
-i=0
-for drop in "${dropouts[@]}"; do
-   folder=/Users/jchevall/JWST/Simulations/XDF/${drop}_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian/${MC_run}/ETC-simulations
-   if [ -d ${folder}/ETC-output ] ; then
-     rm -r ${folder}/ETC-output
-   fi
+root_folder="/home/jchevall/JWST/Simulations/XDF"
+folder_name="ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_weights_mass_SFR_logU"
 
-   input_file=/Users/jchevall/JWST/Simulations/XDF/${drop}_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian/${MC_run}/input-SEDs/input_SEDs_${MC_run}.fits
+for MC_run in "${MC_runs[@]}"; do
+  i=0
+  for drop in "${dropouts[@]}"; do
+     folder=${root_folder}/${drop}_DROPOUTS/${folder_name}/MC_${MC_run}/ETC-simulations
+     if [ -d ${folder}/ETC-output ] ; then
+       rm -r ${folder}/ETC-output
+     fi
 
-  ./make_ETC_simulations.py -i ${input_file} -o ${folder} --exposures ${nbexp} --filters ${filters} --gratings ${gratings} --sersic ${sersic} --effective-radius ${effective_radii[$i]} --nproc ${nproc}
-  i=$((i + 1))
+     input_file=${root_folder}/${drop}_DROPOUTS/${folder_name}/MC_${MC_run}/input-SEDs-IRAC/input_SEDs_MC_${MC_run}.fits
+
+    ./make_ETC_simulations.py -i ${input_file} -o ${folder} --exposures ${nbexp} --filters ${filters} --gratings ${gratings} --sersic ${sersic} --effective-radius shibuya+2015 --nproc ${nproc}
+    i=$((i + 1))
+  done
 done
 
 # B-band dropouts
