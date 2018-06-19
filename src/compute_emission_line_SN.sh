e12e90a4e159730f9f380c9f92b1243d40a13c8f
Jacopo Chevallard <jacopo.chevallard@gmail.com>
2018-06-19 16:11:15 +0200

    Added bash script for the calculation of emission lines S/N

---
+++ src/compute_emission_line_SN.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+declare -a dropouts=("B" "V" "I" "Z" "Y")
+MC_run="MC_0"
+
+folder="ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_weights_mass_SFR_logU"
+config_json="/Users/jchevall/JWST/Simulations/XDF/config.json"
+script="compute_emission_line_SN.py"
+
+for drop in "${dropouts[@]}"; do
+    _folder="/Users/jchevall/JWST/Simulations/XDF/${drop}_DROPOUTS/ineb_Jan16_logU_xid_delayed_SFR-Gaussian_max_age-Gaussian_weights_mass_SFR_logU/${MC_run}/ETC-simulations"
+    ./${script}  --folder ${_folder} --json-file /Users/jchevall/JWST/src/emission_lines_SN_config_PRISM.json
+done
